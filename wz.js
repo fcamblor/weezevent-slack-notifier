1fffeccab7bff49e0cdab347bfbef845b13ab956
fcamblor <frederic.camblor@4sh.fr>
2016-06-21 14:40:24 +0200

    Created weezevent lib js file allowing to retrieve access token & fetch tickets

---
+++ wz.js
@@ -0,0 +1,57 @@
+var rp = require('request-promise');
+var Promise = require('bluebird');
+
+function WeezEvent(opts){
+    var self = this;
+
+    self.wz_user = opts.wzUser;
+    self.wz_pwd = opts.wzPwd;
+    self.wz_api = opts.wzApi;
+    self.wz_evt_id = opts.wzEventId;
+
+    self.wz_access_token = null;
+};
+
+WeezEvent.prototype.init = function(opts){
+    var self = this;
+    
+    // Retrieving weezevent access token given user/password
+    return rp({
+        uri: 'https://api.weezevent.com/auth/access_token',
+        qs: {
+            username: self.wz_user,
+            password: self.wz_pwd,
+            api_key: self.wz_api
+        },
+        json: true
+    }).then(function(result){
+        self.wz_access_token = result.accessToken;
+        
+        console.log('Weezevent access token retrieved : %s', result.accessToken);
+        return Promise.resolve();
+    }).catch(function(err){
+        console.error("Error while retrieving Weezevent Access Token : %s", err);
+        return Promise.rejected("Error while retrieving Weezevent Access Token");
+    });
+};
+
+
+WeezEvent.prototype.fetchWZTickets = function() {
+    var self = this;
+    
+    return rp({
+        uri: 'https://api.weezevent.com/tickets',
+        qs: {
+            access_token: self.wz_access_token,
+            api_key: self.wz_api,
+            id_event: self.wz_evt_id
+        },
+        json: true
+    }).then(function (results) {
+        console.log(JSON.stringify(results));
+        return results;
+    });
+};
+
+
+module.exports = WeezEvent;
