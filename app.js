25a44ffd26201c0d2d46e2ae53fb12b4ab62556e
fcamblor <frederic.camblor@4sh.fr>
2016-06-21 14:44:02 +0200

    Calling we.init() prior to starting the server + provided endpoint to fetch we tickets

--- app.js
+++ app.js
@@ -1,18 +1,34 @@
 var restify = require('restify');
+var Promise = require('bluebird');
+var WeezEvent = require('./wz');
+
+var we = new WeezEvent({
+  wzUser: process.env.WZ_USER,
+  wzPwd: process.env.WZ_PWD,
+  wzApi: process.env.WZ_API,
+  wzEventId: process.env.WZ_EVT_ID
+});
 
 var server = restify.createServer({
   name: 'weezevent-slack-notifier',
   version: '1.0.0'
 });
-server.use(restify.acceptParser(server.acceptable));
-server.use(restify.queryParser());
-server.use(restify.bodyParser());
+server.get('/checkTickets', function (req, res, next) {
+  Promise.all([
+    we.fetchWZTickets()
+  ]).then(function(wzTickets){
+    console.log("WZTickets : %s", JSON.stringify(wzTickets));
 
-server.get('/echo/:name', function (req, res, next) {
-  res.send(req.params);
+  });
   return next();
 });
 
-server.listen(parseInt(process.env.PORT, 10) || 8080, function () {
-  console.log('%s listening at %s', server.name, server.url);
+
+we.init().then(function(){
+  server.use(restify.acceptParser(server.acceptable));
+  server.use(restify.queryParser());
+  server.use(restify.bodyParser());
+  server.listen(parseInt(process.env.PORT, 10) || 8080, function () {
+    console.log('%s listening at %s', server.name, server.url);
+  });
 });
