9d7ded5c37e54c384136498a0ad1615d495b5541
fcamblor <frederic.camblor@4sh.fr>
2016-06-21 23:49:24 +0200

    Improved a bit message displayed in slack when we have only 1 ticket type (most of the cases)

--- msgProducer.js
+++ msgProducer.js
@@ -3,31 +3,46 @@ var _ = require('lodash');
 function SlackMessageProducer(){
 };
 
+function writeParticipants(participants) {
+    return _.map(participants, function(participant){
+        return participant.first_name+" "+participant.last_name+(participant.company?" ("+participant.company+")":"");
+    }).join(", ");
+};
+
 SlackMessageProducer.prototype.produceMessageFrom = function(persistedParticipants, updatedBdxioParticipants){
     var thresholdDate = persistedParticipants.latest_creation?Date.parse(persistedParticipants.latest_creation):0;
 
-    var newParticipantsCount = 0;
-    var msg = _(updatedBdxioParticipants)
+    var newParticipantsByTicket = _(updatedBdxioParticipants)
         // First, filtering every fetched participants already present in persisted participants
         .filter(function(bdxioParticipant){
             return Date.parse(bdxioParticipant.create_date) > thresholdDate;
-        // Grouping by ticket type
-        }).groupBy('ticket')
-        .reduce(function(result, value, key) {
+            // Grouping by ticket type
+        }).groupBy('ticket').value();
+
+    var msg = ':tada:',
+        ticketTypes = _.keys(newParticipantsByTicket),
+        newParticipantsCount = _(newParticipantsByTicket).values().flatten().value().length,
+        plural = newParticipantsCount>=2;
+
+    if(ticketTypes.length === 0) {
+        return null;
+    } else if(ticketTypes.length === 1) {
+        msg += newParticipantsCount+" place"+(plural?"s":"")+" de type "+ticketTypes[0]+" "+(plural?"viennent":"vient")+" d'être vendue"+(plural?"s":"")+" : "+writeParticipants(newParticipantsByTicket[ticketTypes[0]]);
+    } else {
+        msg += newParticipantsCount+" place"+(plural?"s viennent":" vient")+" d'être vendue"+(plural?"s":"")+" répartie"+(plural?"s":"")+" comme suit :\n";
+        msg += _.reduce(newParticipantsByTicket, function(result, value, key) {
             var plural = value.length>=2;
-            result += "- "+value.length+" place"+(plural?"s":"")+" "+key+" : "+(_.map(value, function(participant){
-                return participant.first_name+" "+participant.last_name+(participant.company?" ("+participant.company+")":"");
-            }).join(", "))+"\n";
+            result += "- "+value.length+" place"+(plural?"s":"")+" "+key+" : "+writeParticipants(value)+"\n";
             newParticipantsCount += value.length;
             return result;
         }, "");
-    
+    }
+
     if(newParticipantsCount === 0) {
         return null;
     }
-    
-    var plural = newParticipantsCount>=2;
-    return ":tada:"+newParticipantsCount+" place"+(plural?"s viennent":" vient")+" d'être vendue"+(plural?"s":"")+" répartie"+(plural?"s":"")+" comme suit :\n"+msg+":tada::tada::tada:";
+
+    return msg+"\n:tada::tada::tada:";
 };
 
 SlackMessageProducer.prototype.convertWZParticipantsToBDXIOParticipants = function(wzParticipants, wzTickets) {
